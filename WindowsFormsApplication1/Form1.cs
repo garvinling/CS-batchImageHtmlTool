dc2b88ce6dfcee84d145ff900340ce4f427ec248
Garvin <gling8313@gmail.com>
2014-03-25 00:02:43 -0700

    Get filetypes from directory
    Checks folder dir for .jpg and returns # of found files with the
    matching extensions.

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace WindowsFormsApplication1
 {
@@ -19,25 +20,19 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
+            String dir = "";
+            String folderName = "";
+            Console.WriteLine("User browsing for files.\n");
 
-            Console.Write("User browsing for files.\n");
+            dir = getFolderDirectory();                 
 
-            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
-            DialogResult result = folderBrowser.ShowDialog();
-            String folderDirectory = "";
-
-
-            if (result == DialogResult.OK)
+            if (dir != null)
             {
-                //Get the file path here
-                folderDirectory = folderBrowser.SelectedPath;
-                Console.Write("Path is: " + folderDirectory);
-
+                folderName = getFolderName(dir);        //get selected directory name
             }
 
-
-            //Get Directory Name
-
+            getAllJPGFiles(dir);
+  
             //Check that folder contains only .JPGs. otherwise only store JPG paths.
 
             //Set name of files.   directoryName_#
@@ -46,40 +41,76 @@ namespace WindowsFormsApplication1
 
             //output html with filename to .txt on desktop 
 
+        }//end Browse Onclick
 
 
 
-
-
-
-
-
-
-
-
-            /**
-            Console.Write("User browsing for files.");
-            
-            OpenFileDialog fileDialog = new OpenFileDialog();
-            try
+        /*
+         * getFolderDirectory prompts user with folderbrowser and returns directory path of the selected folder
+         * 
+         * */
+        private String getFolderDirectory()
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            DialogResult result = folderBrowser.ShowDialog();
+            String folderDirectory = "";
+            if (result == DialogResult.OK)
             {
-                if (fileDialog.ShowDialog() == DialogResult.OK)
-                {
-                    Console.Write("Dialog OK.");
-                    String path = fileDialog.FileName;
-                    Console.Write("Path is:" + path);
-                }
+                //Get the file path here
+                folderDirectory = folderBrowser.SelectedPath;
+                return folderDirectory;
             }
-            catch (Exception ex)
-            {
 
+            return null;
+        }//end getFolderDirectory()
+
+
+
+
+        /*
+         * getFolderName() parses the directory string and returns the folder-name
+         * 
+         * */
+        private String getFolderName(String folderDirectory)
+        {
+            String folderName = "";
+
+            Console.WriteLine("Path is: " + folderDirectory);
+            int dirNameIndex = folderDirectory.LastIndexOf('\\');
+            int lastDirNameIndex = folderDirectory.Length;
+            int length = lastDirNameIndex - dirNameIndex;
+            folderName = folderDirectory.Substring(dirNameIndex + 1, length - 1);
+            Console.WriteLine("Folder Name: " + folderName);
+
+            return folderName;
+        }
+
+     
+        /*
+         * getAllJPGFiles()
+         * 
+         * */
+        private void getAllJPGFiles(String dir)
+        {
+            string[] filePaths = Directory.GetFiles(@dir, "*.jpg");
+            Console.WriteLine("The number of files ending with .jpg is {0}.", filePaths.Length);
+
+            foreach (string dirs in filePaths)
+            {
+                Console.WriteLine("Array: " +dir);
             }
-             **/
+
 
 
 
         }
 
-     
+
+
+
+
+
     }
+
+
 }
